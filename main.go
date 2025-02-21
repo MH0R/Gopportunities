247097c4bb3be3cdd5aa0f94a3a57690ca35ba63
Thalles Oliveira <66585100+MH0R@users.noreply.github.com>
2025-02-21 16:56:05 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"github.com/MH0R/Gopportunities/config"
 	"github.com/MH0R/Gopportunities/router"
 )
 
+var (
+	logger config.Logger
+)
+
 func main() {
+	logger = *config.GetLogger("main")
+	// Initialize Config
+	err := config.Init()
+	if err != nil {
+		logger.Errf("Error initializing config: %v", err)
+		return
+	}
 	router.Initialize()
 }
--- router/handler/handler.go
+++ router/handler/handler.go
@@ -1,5 +1,1 @@
 package handler
-
-func handler() {
-	// This is a placeholder for the handler function.
-}
