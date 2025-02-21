addc17a8617f9ae15dac3366aca3c903a0a777fd
Thalles Oliveira <66585100+MH0R@users.noreply.github.com>
2025-02-21 20:44:31 -0300

    feat: add GET methods to list openings

--- router/handler/showOpening.go
+++ router/handler/showOpening.go
@@ -1,13 +1,31 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/MH0R/Gopportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ShowOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "POST",
-	})
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id",
+			"queryParameter").Error())
+		return
+	}
+
+	opening := schemas.Opening{}
+
+	//Find opening
+
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("opening with id: %s not found", id))
+		return
+	}
+
+	//Show Opening
+	sendSuccess(ctx, "show-opening", opening)
+
 }
